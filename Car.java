923ec2f9538a97ab2e196ce2a66a3ba4cffa43d9
Kannan-75 <145036127+Kannan-75@users.noreply.github.com>
2023-09-14 21:34:30 +0530

    Add files via upload
     Day1Assingment

---
+++ Car.java
@@ -0,0 +1,60 @@
+package week1.day1;
+
+public class Car {
+	 public void startcar() {
+		 System.out.println("startcar");
+	 }
+	 
+	 public void  closethedoor() {
+		 System.out.println("closethedoor");
+	 }
+	 
+	 public void  putgear() {
+		 System.out.println("putgear");
+	 }
+	 
+	 public void  openwindow() {
+		 System.out.println("openwindow");
+	 }
+	 
+	 public void  changegear() {
+		 System.out.println("changegear");
+	 }
+	 
+	 public void  applybrake() {
+		 System.out.println("applybrake");
+	 }
+	 
+	 public void  adjustsidemirror() {
+		 System.out.println("adjustsidemirror");
+	 } 
+	 
+	 public void  soundhorn() {
+		 System.out.println("soundhorn");
+	 }
+	 
+	 public void  turnonheadlights() {
+		 System.out.println("turnonheadlights");
+	 }
+	 public static void main(String[] args) {
+		Car volvo= new Car();
+		volvo. startcar();
+		volvo. closethedoor();
+		volvo. putgear();
+		volvo. openwindow();
+		volvo. changegear();
+		volvo. applybrake();
+		volvo. adjustsidemirror();
+		volvo. soundhorn();
+		volvo. turnonheadlights();
+		
+		
+	}
+	 
+
+	 
+	 
+	 
+	 
+	 
+}
---
+++ Mobile.java
@@ -0,0 +1,67 @@
+package week1.day1;
+
+public class Mobile {
+	public void presspowerbutton() {
+		System.out.println("pressPowerButton");
+	}
+	
+	public void swipelockscreen() {
+		System.out.println("swipeLockscreen");
+	}
+	
+	public void enterPassword() {
+		System.out.println("enterPassword");
+	}
+	
+	public void openCamera() {
+		System.out.println("openCamera");
+	}
+	
+	public void clickPhotos() {
+		System.out.println("clickPhotos");
+	}
+	
+	public void openWhatsapp() {
+		System.out.println("openWhatsapp");
+	}
+	
+	public void sendMessage() {
+		System.out.println("sendMessage");
+	}
+	
+	public void openInstagram() {
+		System.out.println("openInstagram");
+	}
+	
+	public void sendMemes() {
+		System.out.println("sendMemes");
+	}
+	
+	public static void main(String[] args) {
+		Mobile redmi= new Mobile();
+		redmi.presspowerbutton();
+		redmi.swipelockscreen();
+		redmi.enterPassword();
+		redmi.openCamera();
+		redmi.clickPhotos();
+		redmi.openWhatsapp();
+		redmi.sendMessage();
+		redmi.openInstagram();
+		redmi.sendMemes();
+		
+				
+		
+	}
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+
+	
+}
